a0c96822764ea5c9b1ca01095a844c9cc573be87
Hannah <okami118@Hannah-Springers-MacBook-Pro.local>
2016-05-12 20:49:38 -0500

    changed port information to prevent crashing on heroku.

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var felines = require('./routes/felines');
 
-app.set('port', 5000);
+app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
